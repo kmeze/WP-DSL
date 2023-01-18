a69dd416739eb41ee97c3eac7697fb4a94775634
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-18 10:06:35 +0100

    Implemented column creation and mapping for ShortStringProperty concept

--- KMeze.Rhetos.WordPress.PluginGenerator/WPConcepts/Dom/ShortStringPropertyCodeGenerator.cs
+++ KMeze.Rhetos.WordPress.PluginGenerator/WPConcepts/Dom/ShortStringPropertyCodeGenerator.cs
@@ -18,6 +18,14 @@ namespace KMeze.Rhetos.WordPress.PluginGenerator
             string snippet = $@"public ?string ${info.Name} = null;
     ";
             codeBuilder.InsertCode(snippet, EntityCodeGenerator.PropertyTag, info.Entity);
+
+            snippet = $@",{info.Name} VARCHAR(256)
+                ";
+                        codeBuilder.InsertCode(snippet, EntityCodeGenerator.ColumnTag, info.Entity);
+
+            snippet = $@"$entity->{info.Name} = $row->{info.Name};
+                ";
+                        codeBuilder.InsertCode(snippet, EntityCodeGenerator.ColumnMapTag, info.Entity);
         }
     }
 }
