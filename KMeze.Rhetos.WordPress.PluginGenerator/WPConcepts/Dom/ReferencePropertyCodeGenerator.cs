b77e604a02d22c1839e7d9b1b6e38913a0158bd2
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-19 06:44:59 +0100

    Reference concept basic implementation (without creating foreign key)

---
+++ KMeze.Rhetos.WordPress.PluginGenerator/WPConcepts/Dom/ReferencePropertyCodeGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.Rhetos.WordPress.PluginGenerator
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(ReferencePropertyInfo))]
+    public class ReferencePropertyCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (ReferencePropertyInfo)conceptInfo;
+
+            string snippet = $@"public ?int ${info.Name}_id = null;
+    ";
+            codeBuilder.InsertCode(snippet, EntityCodeGenerator.PropertyTag, info.Entity);
+
+            snippet = $@",{info.Name}_id BIGINT(20) DEFAULT NULL
+                        ";
+            codeBuilder.InsertCode(snippet, EntityCodeGenerator.ColumnTag, info.Entity);
+
+            snippet = $@"$entity->{info.Name}_id = is_null($row->{info.Name}_ID) ? null : (int) $row->{info.Name}_ID;
+        ";
+            codeBuilder.InsertCode(snippet, EntityCodeGenerator.ColumnMapTag, info.Entity);
+
+            snippet = $@",KEY ind_{info.Entity.Name}_{info.Name}_id ({info.Name}_id)
+                        ";
+            codeBuilder.InsertCode(snippet, EntityCodeGenerator.KeyMapTag, info.Entity);
+
+        }
+    }
+}
