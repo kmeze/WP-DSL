4e88362b780ec890f1040e54c5f7cbb6b69358f7
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-20 18:49:45 +0100

    Added Pinia store generator

---
+++ KMeze.Rhetos.WordPress.PluginGenerator/JsSdk/Plugins/Dom/DataStructure/EntityCodeGenerator.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.Rhetos.WordPress.PluginGenerator.JsSdk
+{
+    [Export(typeof(IWPPluginJsSdkConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(EntityInfo))]
+    public class EntityCodeGenerator : IWPPluginJsSdkConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (EntityInfo)conceptInfo;
+
+            // Generate entity state in Pinia store
+            string snippet = $@"{info.Name}: [],
+            ";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.PiniaStoreStateTag, info.WPPlugin);
+
+            // Generate entity actions in Pinia store
+            snippet = $@"async fetch{info.Name}() {{
+            const res = await axios.get(this.apiUrl).then(res => {{
+                this.{info.Name} = res.data;
+            }})
+        }},
+        ";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.PiniaStoreActionTag, info.WPPlugin);
+        }
+    }
+}
---
+++ KMeze.Rhetos.WordPress.PluginGenerator/JsSdk/Plugins/Dom/SimpleBusinessLogic/WPPluginCodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.Rhetos.WordPress.PluginGenerator.JsSdk
+{
+    [Export(typeof(IWPPluginJsSdkConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(WPPluginInfo))]
+    public class WPPluginCodeGenerator : IWPPluginJsSdkConceptCodeGenerator
+    {
+        public static readonly CsTag<WPPluginInfo> PiniaStoreStateTag = "PiniaStoreState";
+        public static readonly CsTag<WPPluginInfo> PiniaStoreActionTag = "PiniaStoreAction";
+
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (WPPluginInfo)conceptInfo;
+
+            codeBuilder.InsertCodeToFile(
+$@"import {{ defineStore }} from 'pinia'
+import axios from 'axios'
+
+export const use{info.Name}Store = defineStore('{info.Name}', {{
+    state: () => {{
+        return {{
+            apiUrl: '',
+            {PiniaStoreStateTag.Evaluate(info)}
+        }}
+    }},
+    actions: {{
+        {PiniaStoreActionTag.Evaluate(info)}
+    }},
+}})", $"{Path.Combine(Path.Combine(Path.Combine(Path.Combine("WordPress", info.Name), "src"), "stores"), info.Name + "Store")}");
+        } 
+    }
+}
