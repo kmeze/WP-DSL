14c4c339f17fc51c51004c5b2f6f5a886c9218dc
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-20 18:50:11 +0100

    Added demo app

---
+++ VueApp/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {useTestPluginStore} from '@/stores/TestPluginStore'
+
+const testPluginStore = useTestPluginStore()
+testPluginStore.apiUrl = 'http://localhost:8000/wp-json/TestPlugin/v1/TestEntity'
+
+</script>
+
+<template>
+  <main>
+    <h1>TestEntity Count: {{testPluginStore.TestEntity.length}}</h1>
+    <button type="button" @click="testPluginStore.fetchTestEntity()">Fetch</button>
+    <li v-for="item in testPluginStore.TestEntity">
+      {{ item }}
+    </li>
+  </main>
+</template>
+
+<style scoped>
+</style>
