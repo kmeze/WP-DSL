4f2407d5653fe50ed98a6418da2af1447bedad51
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-26 12:16:10 +0100

    Added Role concept

---
+++ KMeze.WordPressDSL/Plugins/Dom/SimpleBusinessLogic/RoleCodeGenerator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WordPressDSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(RoleInfo))]
+    public class RoleCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (RoleInfo)conceptInfo;
+
+            string snippet = $@"add_role( '{info.WPPlugin.Name}_{info.Slug}', '{info.Name}' );
+
+";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.ActivationHookTag, info.WPPlugin);
+
+            snippet = $@"remove_role( '{info.WPPlugin.Name}_{info.Slug}' );
+
+";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.DeactivationHookTag, info.WPPlugin);
+
+        }
+    }
+}
