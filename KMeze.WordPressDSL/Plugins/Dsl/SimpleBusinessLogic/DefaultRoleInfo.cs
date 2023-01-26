87c8bddf1e060f1853678785fab17da8c0daeb36
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-26 17:55:52 +0100

    Added DefaultRole concept

---
+++ KMeze.WordPressDSL/Plugins/Dsl/SimpleBusinessLogic/DefaultRoleInfo.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+
+namespace KMeze.WordPressDSL
+{
+    /// <summary>
+    /// Represents custom entity.
+    /// </summary>
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("DefaultRole")]
+    public class DefaultRoleInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public WPPluginInfo WPPlugin { get; set; }
+
+        [ConceptKey]
+        public RoleInfo Role { get; set; }
+    }
+}
