5e718daf5ea36cd8999baf5f9651745cc6352782
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-29 11:51:27 +0100

    Create DataStructureCodeGenerator.cs

---
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/DataStructureCodeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Newtonsoft.Json.Linq;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(DataStructureInfo))]
+    public class DataStructureCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public static readonly CsTag<DataStructureInfo> ClassPropertyTag = "ClassProperty";
+        public static readonly CsTag<DataStructureInfo> ClassConstructorTag = "ClassConstructor";
+        public static readonly CsTag<DataStructureInfo> ClassMethodTag = "ClassMethod";
+
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (DataStructureInfo)conceptInfo;
+
+            string snippet = $@"class {info.WPPlugin.Name}_{info.Name} {{
+    {ClassPropertyTag.Evaluate(info)}
+
+    public function __construct() {{
+        {ClassConstructorTag.Evaluate(info)}
+    }}
+
+    {ClassMethodTag.Evaluate(info)}
+}}
+
+";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.BodyTag, info.WPPlugin);
+        }
+    }
+}
