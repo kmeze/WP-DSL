4f2794ae2fe9ec27a01f7bbe94896e1b6408dd22
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-29 15:59:55 +0100

    Added Repository concept

--- KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityCodeGenerator.cs
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityCodeGenerator.cs
@@ -22,42 +22,12 @@ namespace KMeze.WP.DSL
     ";
             codeBuilder.InsertCode(snippet, DataStructureCodeGenerator.ClassPropertyTag, info);
 
+            snippet = $@"$dataStructure->id = (int) $object->ID;
+    ";
+            codeBuilder.InsertCode(snippet, DataStructureCodeGenerator.ClassParsePropertyTag, info);
+
             // Generate entity class
-            snippet = $@"class {info.WPPlugin.Name}_{info.Name}_Repository {{
-    public function select_{info.Name}() {{
-        global $wpdb;
-        $table_name = $wpdb->prefix . '{info.WPPlugin.Name}_{info.Name}';
-
-        return $wpdb->get_results( ""SELECT * FROM {{$table_name}};"" );
-    }}
-
-    public function select_{info.Name}_by_ID( int $id ) {{
-        global $wpdb;
-        $table_name = $wpdb->prefix . '{info.WPPlugin.Name}_{info.Name}';
-
-        return $wpdb->get_row( ""SELECT * FROM {{$table_name}} WHERE ID={{$id}};"" );
-    }}
-
-    public function insert_{info.Name}( array $data ) {{
-        global $wpdb;
-        $table_name = $wpdb->prefix . '{info.WPPlugin.Name}_{info.Name}';
-        $wpdb->insert( $table_name, $data );
-
-        return $wpdb->insert_id;
-    }}
-
-    public function update_{info.Name}( int $id, array $data ) {{
-	    global $wpdb;
-	    $table_name = $wpdb->prefix . '{info.WPPlugin.Name}_{info.Name}';
-	    $wpdb->update( $table_name, $data, array( 'id' => $id ) );
-    }}
-
-    public function delete_{info.Name}( int $id ) {{
-	    global $wpdb;
-	    $table_name = $wpdb->prefix . '{info.WPPlugin.Name}_{info.Name}';
-	    $wpdb->delete( $table_name, array( 'id' => $id ) );
-    }}
-}}
+            snippet = $@"
 
 ";
             codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.BodyTag, info.WPPlugin);
@@ -154,6 +124,11 @@ add_action( 'rest_api_init', function () {{
             var newConcepts = new List<IConceptInfo>();
 
             newConcepts.Add(new DbDeltaInfo
+            {
+                DataStructure = conceptInfo
+            });
+
+            newConcepts.Add(new RepositoryInfo
             {
                 DataStructure = conceptInfo
             });
---
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/RepositoryCodeGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(RepositoryInfo))]
+    public class RepositoryCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (RepositoryInfo)conceptInfo;
+
+            string snippet = $@"class {info.DataStructure.WPPlugin.Name}_{info.DataStructure.Name}_Repository {{
+    public function select_{info.DataStructure.Name}() {{
+        global $wpdb;
+        $table_name = $wpdb->prefix . '{info.DataStructure.WPPlugin.Name}_{info.DataStructure.Name}';
+
+        return $wpdb->get_results( ""SELECT * FROM {{$table_name}};"" );
+    }}
+
+    public function select_{info.DataStructure.Name}_by_ID( int $id ) {{
+        global $wpdb;
+        $table_name = $wpdb->prefix . '{info.DataStructure.WPPlugin.Name}_{info.DataStructure.Name}';
+
+        return $wpdb->get_row( ""SELECT * FROM {{$table_name}} WHERE ID={{$id}};"" );
+    }}
+
+    public function insert_{info.DataStructure.Name}( array $data ) {{
+        global $wpdb;
+        $table_name = $wpdb->prefix . '{info.DataStructure.WPPlugin.Name}_{info.DataStructure.Name}';
+        $wpdb->insert( $table_name, $data );
+
+        return $wpdb->insert_id;
+    }}
+
+    public function update_{info.DataStructure.Name}( int $id, array $data ) {{
+	    global $wpdb;
+	    $table_name = $wpdb->prefix . '{info.DataStructure.WPPlugin.Name}_{info.DataStructure.Name}';
+	    $wpdb->update( $table_name, $data, array( 'id' => $id ) );
+    }}
+
+    public function delete_{info.DataStructure.Name}( int $id ) {{
+	    global $wpdb;
+	    $table_name = $wpdb->prefix . '{info.DataStructure.WPPlugin.Name}_{info.DataStructure.Name}';
+	    $wpdb->delete( $table_name, array( 'id' => $id ) );
+    }}
+}}
+
+";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.BodyTag, info.DataStructure.WPPlugin);
+        }
+    }
+}
