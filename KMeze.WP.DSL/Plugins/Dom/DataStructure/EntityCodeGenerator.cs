73bc011588cdbc276a0491e85719e226e60c9e3c
Kresimir Meze <kresimir.meze@gmail.com>
2023-01-30 07:23:16 +0100

    Generating of REST_Controller class move to EntityRestControllerCodeGenerator plugin

--- KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityCodeGenerator.cs
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityCodeGenerator.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.Composition;
 using Rhetos.Compiler;
 using Rhetos.Dom.DefaultConcepts;
 using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
 using Rhetos.Extensibility;
 
 namespace KMeze.WP.DSL
@@ -11,9 +12,6 @@ namespace KMeze.WP.DSL
     [ExportMetadata(MefProvider.Implements, typeof(EntityInfo))]
     public class EntityCodeGenerator : IWPPluginConceptCodeGenerator
     {
-        public static readonly CsTag<DataStructureInfo> ColumnMapTag = "ColumnMap";
-        public static readonly CsTag<DataStructureInfo> AuthorizationTag = "Authorization";
-
         public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
         {
             var info = (EntityInfo)conceptInfo;
@@ -23,90 +21,8 @@ namespace KMeze.WP.DSL
             codeBuilder.InsertCode(snippet, DataStructureCodeGenerator.ClassPropertyTag, info);
 
             snippet = $@"$dataStructure->id = (int) $object->ID;
-    ";
+        ";
             codeBuilder.InsertCode(snippet, DataStructureCodeGenerator.ClassParsePropertyTag, info);
-
-            // Generate entity REST controller and register routes
-            snippet = $@"class {info.WPPlugin.Name}_{info.Name}_REST_Controller {{
-    public function register_routes() {{
-        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}', array(
-            'methods'             => 'GET',
-            'callback'            => array( $this, 'get_items' ),
-            'permission_callback' => array( $this, 'permissions_check' ),
-        ) );
-
-        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}/(?P<id>[\d]+)', array(
-            'methods'             => 'GET',
-            'callback'            => array( $this, 'get_item' ),
-            'permission_callback' => array( $this, 'permissions_check' ),
-        ) );
-
-        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}', array(
-            'methods'             => 'POST',
-            'callback'            => array( $this, 'post_item' ),
-            'permission_callback' => array( $this, 'permissions_check' ),
-        ) );
-
-        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}/(?P<id>[\d]+)', array(
-            'methods'             => 'PUT',
-            'callback'            => array( $this, 'put_item' ),
-            'permission_callback' => array( $this, 'permissions_check' ),
-        ) );
-
-        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}/(?P<id>[\d]+)', array(
-            'methods'             => 'DELETE',
-            'callback'            => array( $this, 'delete_item' ),
-            'permission_callback' => array( $this, 'permissions_check' ),
-        ) );
-    }}
-
-    public function permissions_check( $request ): bool {{
-        {AuthorizationTag.Evaluate(info)}
-
-        return false;
-    }}
-
-    private function prepare_item_for_response( $row ) {{
-        $entity     = new {info.WPPlugin.Name}_{info.Name}();
-        $entity->id = (int) $row->ID;
-        {ColumnMapTag.Evaluate(info)}
-
-        return $entity;
-    }}
-
-    public function get_items( $request ): array {{
-        return {info.WPPlugin.Name}_{info.Name}::get_results();
-
-
-	    return array_map( function ( $row ) {{
-	        return $this->prepare_item_for_response( $row );
-	    }}, ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->select_{info.Name}() );
-    }}
-
-    public function get_item( $request ) {{
-        return $this->prepare_item_for_response( ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->select_{info.Name}_by_ID( $request->get_param( 'id' ) ) );
-    }}
-
-    public function post_item( $request ) {{
-        return ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->insert_{info.Name}( $request->get_json_params() );
-    }}
-
-    public function put_item( $request ) {{
-        return ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->update_{info.Name}( $request->get_param( 'id' ), $request->get_json_params() );
-    }}
-
-    public function delete_item( $request ) {{
-        return ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->delete_{info.Name}( $request->get_param( 'id' ) );
-    }}
-}}
-
-add_action( 'rest_api_init', function () {{
-    $controller = new {info.WPPlugin.Name}_{info.Name}_REST_Controller();
-    $controller->register_routes();
-}} );
-
-";
-            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.BodyTag, info.WPPlugin);
         }
     }
 
---
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityRestControllerCodeGenerator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(EntityInfo))]
+    public class EntityRestControllerCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public static readonly CsTag<EntityInfo> AuthorizationTag = "Authorization";
+
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (EntityInfo)conceptInfo;
+
+            string snippet = $@"class {info.WPPlugin.Name}_{info.Name}_REST_Controller {{
+    public function register_routes() {{
+        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}', array(
+            'methods'             => 'GET',
+            'callback'            => array( $this, 'get_items' ),
+            'permission_callback' => array( $this, 'permissions_check' ),
+        ) );
+
+        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}/(?P<id>[\d]+)', array(
+            'methods'             => 'GET',
+            'callback'            => array( $this, 'get_item' ),
+            'permission_callback' => array( $this, 'permissions_check' ),
+        ) );
+
+        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}', array(
+            'methods'             => 'POST',
+            'callback'            => array( $this, 'post_item' ),
+            'permission_callback' => array( $this, 'permissions_check' ),
+        ) );
+
+        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}/(?P<id>[\d]+)', array(
+            'methods'             => 'PUT',
+            'callback'            => array( $this, 'put_item' ),
+            'permission_callback' => array( $this, 'permissions_check' ),
+        ) );
+
+        register_rest_route( '{info.WPPlugin.Name}/v1', '/{info.Name}/(?P<id>[\d]+)', array(
+            'methods'             => 'DELETE',
+            'callback'            => array( $this, 'delete_item' ),
+            'permission_callback' => array( $this, 'permissions_check' ),
+        ) );
+    }}
+
+    public function permissions_check( $request ): bool {{
+        {AuthorizationTag.Evaluate(info)}
+
+        return false;
+    }}
+
+    private function prepare_item_for_response( $row ) {{
+        return {info.WPPlugin.Name}_{info.Name}::parse( $row );
+    }}
+
+    public function get_items( $request ): array {{
+	    return array_map( function ( $row ) {{
+	        return $this->prepare_item_for_response( $row );
+	    }}, ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->select_{info.Name}() );
+    }}
+
+    public function get_item( $request ) {{
+        return $this->prepare_item_for_response( ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->select_{info.Name}_by_ID( $request->get_param( 'id' ) ) );
+    }}
+
+    public function post_item( $request ) {{
+        return ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->insert_{info.Name}( $request->get_json_params() );
+    }}
+
+    public function put_item( $request ) {{
+        return ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->update_{info.Name}( $request->get_param( 'id' ), $request->get_json_params() );
+    }}
+
+    public function delete_item( $request ) {{
+        return ( new {info.WPPlugin.Name}_{info.Name}_Repository() )->delete_{info.Name}( $request->get_param( 'id' ) );
+    }}
+}}
+
+add_action( 'rest_api_init', function () {{
+    $controller = new {info.WPPlugin.Name}_{info.Name}_REST_Controller();
+    $controller->register_routes();
+}} );
+
+";
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.BodyTag, info.WPPlugin);
+        }
+    }
+}
