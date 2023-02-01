bbaeac37c4b03b3cbabb04552608f646526a6145
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-01 09:23:26 +0100

    Refactoring: Code moved to FromReferenceInfo file

--- KMeze.WP.DSL/Plugins/Dsl/DataStructure/FromReference.cs
+++ KMeze.WP.DSL/Plugins/Dsl/DataStructure/FromReference.cs
@@ -13,4 +13,41 @@ namespace KMeze.WP.DSL
         [ConceptKey]
         public ReferencePropertyInfo SourceReferencePropertyInfo { get; set; }
     }
+
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("FromReference")]
+    public class FromNamedReferenceInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public ListInfo List { get; set; }
+
+        [ConceptKey]
+        public string Name { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class FromNamedReferenceMacro : IConceptMacro<FromNamedReferenceInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(FromNamedReferenceInfo conceptInfo, IDslModel existingConcepts)
+        {
+            ReferencePropertyInfo rpi = existingConcepts.FindByReference<ReferencePropertyInfo>(ci => ci.DataStructure, conceptInfo.List.Source)
+               .Where(ci => ci.Name == conceptInfo.Name)
+               .FirstOrDefault();
+
+            var reference = new ReferencePropertyInfo
+            {
+                DataStructure = conceptInfo.List.Source,
+                Name = conceptInfo.Name,
+                ReferencedDataStructure = rpi.ReferencedDataStructure,
+            };
+
+            var fromReferenceInfo = new FromReferenceInfo
+            {
+                List = conceptInfo.List,
+                SourceReferencePropertyInfo = reference,
+            };
+
+            return new IConceptInfo[] { reference, fromReferenceInfo };
+        }
+    }
 }
