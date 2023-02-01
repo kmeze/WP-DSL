20f526ff19c595006dc3a72d820a0d54105f6227
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-01 09:07:36 +0100

    Added TakeFromReferenceMacro class

--- KMeze.WP.DSL/Plugins/Dsl/DataStructure/TakeFromReferenceInfo.cs
+++ KMeze.WP.DSL/Plugins/Dsl/DataStructure/TakeFromReferenceInfo.cs
@@ -11,9 +11,32 @@ namespace KMeze.WP.DSL
     public class TakeFromReferenceInfo : IConceptInfo
     {
         [ConceptKey]
-        public FromReferenceInfo Reference { get; set; }
+        public FromReferenceInfo FromReference { get; set; }
 
         [ConceptKey]
-        public PropertyInfo Property { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class TakeFromReferenceMacro : IConceptMacro<TakeFromReferenceInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(TakeFromReferenceInfo conceptInfo, IDslModel existingConcepts)
+        {
+            // FromReference.Reference je što (ReferenceProperty u Source DataStructure)
+            var re = existingConcepts.FindByReference<ReferencePropertyInfo>(ci => ci.DataStructure, conceptInfo.FromReference.List.Source)
+                .Where(ci => ci.Name == conceptInfo.FromReference.SourceReferencePropertyName.Name)
+                .SingleOrDefault();
+
+            var pi = existingConcepts.FindByReference<PropertyInfo>(ci => ci.DataStructure, re.ReferencedDataStructure)
+                .Where(ci => ci.Name == conceptInfo.Name)
+                .SingleOrDefault();
+
+            // TODO: Refactor
+            if (pi is ShortStringPropertyInfo) return new IConceptInfo[] { new ShortStringPropertyInfo { DataStructure = conceptInfo.FromReference.List, Name = $@"{re.Name}_{conceptInfo.Name}" } };
+            if (pi is IntegerPropertyInfo) return new IConceptInfo[] { new IntegerPropertyInfo { DataStructure = conceptInfo.FromReference.List, Name = $@"{re.Name}_{conceptInfo.Name}" } };
+            if (pi is BoolPropertyInfo) return new IConceptInfo[] { new BoolPropertyInfo { DataStructure = conceptInfo.FromReference.List, Name = $@"{re.Name}_{conceptInfo.Name}" } };
+
+            return null;
+        }
     }
 }
