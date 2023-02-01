3505b6f2aace14b9c2ef1883e60cad9a3975db23
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-01 09:14:17 +0100

    Code moved to TakeInfo file

--- KMeze.WP.DSL/Plugins/Dsl/DataStructure/TakeInfo.cs
+++ KMeze.WP.DSL/Plugins/Dsl/DataStructure/TakeInfo.cs
@@ -32,4 +32,36 @@ namespace KMeze.WP.DSL
             return null;
         }
     }
+
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("Take")]
+    public class TakeNamedInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public ListInfo List { get; set; }
+
+        [ConceptKey]
+        public string PropertyName { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class TakeNamedMacro : IConceptMacro<TakeNamedInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(TakeNamedInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var property = new PropertyInfo
+            {
+                DataStructure = conceptInfo.List.Source,
+                Name = conceptInfo.PropertyName,
+            };
+
+            var takeInfo = new TakeInfo
+            {
+                List = conceptInfo.List,
+                SourcePropertyName = property,
+            };
+
+            return new IConceptInfo[] { property, takeInfo };
+        }
+    }
 }
