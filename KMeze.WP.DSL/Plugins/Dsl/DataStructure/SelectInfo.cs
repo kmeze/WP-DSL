c91ccd3ed573ed24024b47741bc10681d4abfba9
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-01 16:06:25 +0100

    Added Select concept

---
+++ KMeze.WP.DSL/Plugins/Dsl/DataStructure/SelectInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("Select")]
+    public class SelectInfo : DataStructureInfo
+    {
+        public string Query { get; set; }
+    }
+}
