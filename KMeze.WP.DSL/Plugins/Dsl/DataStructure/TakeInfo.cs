2a61439e556601cc5e330302618580027b06642b
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-01 05:52:02 +0100

    Create new PropertyInfo for Take concept

--- KMeze.WP.DSL/Plugins/Dsl/DataStructure/TakeInfo.cs
+++ KMeze.WP.DSL/Plugins/Dsl/DataStructure/TakeInfo.cs
@@ -16,4 +16,22 @@ namespace KMeze.WP.DSL
         [ConceptKey]
         public PropertyInfo Property { get; set; }
     }
+
+    [Export(typeof(IConceptMacro))]
+    public class TakeMacro : IConceptMacro<TakeInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(TakeInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var pi = existingConcepts.FindByReference<PropertyInfo>(ci => ci.DataStructure, conceptInfo.Property.DataStructure)
+                .Where(ci => ci.Name == conceptInfo.Property.Name)
+                .SingleOrDefault();
+
+            // TODO: Refactor
+            if (pi is ShortStringPropertyInfo) return new IConceptInfo[] { new ShortStringPropertyInfo { DataStructure = conceptInfo.List, Name = conceptInfo.Property.Name  } };
+            if (pi is IntegerPropertyInfo) return new IConceptInfo[] { new IntegerPropertyInfo { DataStructure = conceptInfo.List, Name = conceptInfo.Property.Name } };
+            if (pi is BoolPropertyInfo) return new IConceptInfo[] { new BoolPropertyInfo { DataStructure = conceptInfo.List, Name = conceptInfo.Property.Name } };
+            
+            return null;
+        }
+    }
 }
