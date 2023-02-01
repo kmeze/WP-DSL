e73a8c9e5e2131ab624d6300a9a9073c8407801a
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-01 17:17:25 +0100

    Added DataStructureAction concept

---
+++ KMeze.WP.DSL/Plugins/Dom/SimpleBusinessLogic/DataStructureActionCodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(DataStructureActionInfo))]
+    public class DataStructureActionCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (DataStructureActionInfo)conceptInfo;
+
+            string snippet = $@"register_rest_route( $this->namespace, $this->resource_name . '/{info.Action.Name}', array(
+            'methods'             => 'GET',
+            'callback'            => array( $this, 'get_{info.Action.Name}' ),
+            'permission_callback' => array( $this, 'item_permissions_check' ),
+        ) );
+
+        ";
+            codeBuilder.InsertCode(snippet, RestControllerCodeGenerator.RestControllerClassRegisterRoutesTag, info.DataStructure);
+
+            snippet = $@"   public function get_{info.Action.Name}( $request ): array {{
+        {info.Action.Script}
+    }}
+
+    ";
+            codeBuilder.InsertCode(snippet, RestControllerCodeGenerator.RestControllerClassMethodTag, info.DataStructure);
+        }
+    }
+}
---
+++ KMeze.WP.DSL/Plugins/Dsl/SimpleBusinessLogic/DataStructureActionInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+
+namespace KMeze.WP.DSL
+{
+    /// <summary>
+    /// Represents custom entity.
+    /// </summary>
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("Action")]
+    public class DataStructureActionInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public DataStructureInfo DataStructure { get; set; }
+
+        [ConceptKey]
+        public ActionInfo Action { get; set; }
+    }
+}
