b22beebc8fd29e66f0a2b26e65c91a337d53dada
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-02 09:40:04 +0100

    Added ActionPost concept

---
+++ KMeze.WP.DSL/Plugins/Dsl/SimpleBusinessLogic/DataStructureActionPostInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("ActionPost")]
+    public class DataStructureActionPostInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public DataStructureInfo DataStructure { get; set; }
+
+        [ConceptKey]
+        public ActionInfo Action { get; set; }
+    }
+}
