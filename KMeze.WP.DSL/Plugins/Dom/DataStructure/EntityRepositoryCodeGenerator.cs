1ff732a7162fd70710d56f3862ed16f39280afdf
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-04 08:04:23 +0100

    Added _filter hook to Entity repository update() method

--- KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityRepositoryCodeGenerator.cs
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/EntityRepositoryCodeGenerator.cs
@@ -79,7 +79,12 @@ namespace KMeze.WP.DSL
     }}
 
     public function update( int $id, array $data ) {{
-	    $this->wpdb->update( $this->{info.Name}_table_name, $data, array( 'ID' => $id ) );
+        $conditions = [];
+        $conditions = apply_filters( '{info.WPPlugin.Name}_{info.Name}_filter', $conditions );
+        $conditions[] = array( 'Name' => 'ID', 'Value' => $id, 'Format' => '%d' );
+        $transformed = $this->transform_conditions($conditions);
+
+	    $this->wpdb->update( $this->{info.Name}_table_name, $data, $transformed['NAME_VALUE'], null, $transformed['ARGS']);
     }}
 
     public function delete( int $id ) {{
