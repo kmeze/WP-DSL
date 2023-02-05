86af8fd293ff127a135e8c2a0ea4c7f592ef4dbe
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-05 18:08:02 +0100

    Register all rest routes in single global action hook callback

--- KMeze.WP.DSL/Plugins/Dsl/DataStructure/DataStructureInfo.cs
+++ KMeze.WP.DSL/Plugins/Dsl/DataStructure/DataStructureInfo.cs
@@ -13,27 +13,4 @@ namespace KMeze.WP.DSL
         [ConceptKey]
         public string Name { get; set; }
     }
-
-    [Export(typeof(IConceptMacro))]
-    public class DataStructureMacro : IConceptMacro<DataStructureInfo>
-    {
-        public IEnumerable<IConceptInfo> CreateNewConcepts(DataStructureInfo conceptInfo, IDslModel existingConcepts)
-        {
-            var callback = new CallbackInfo
-            {
-                WPPlugin = conceptInfo.WPPlugin,
-                Name = $@"_{conceptInfo.Name}_register_routes",
-                Script = $@"( new {conceptInfo.WPPlugin.Name}_{conceptInfo.Name}_REST_Controller() )->register_routes();"
-            };
-
-            var actionHook = new ActionHookWithDefaultPriorityInfo
-            {
-                WPPlugin = conceptInfo.WPPlugin,
-                HookName = "rest_api_init",
-                Callback = callback,
-            };
-
-            return new IConceptInfo[] { callback, actionHook };
-        }
-    }
 }
