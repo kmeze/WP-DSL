ce2f91fb5f38cbf6ee57d41c2e52dc522d34b4d5
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-06 10:19:55 +0100

    Added Required concept

---
+++ KMeze.WP.DSL/Plugins/Dom/SimpleBusinessLogic/RequiredCodeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+using KMeze.WP.DSL;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(RequiredInfo))]
+    public class RequiredCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (RequiredInfo)conceptInfo;
+
+            if (info.Property.DataStructure is EntityInfo)
+            {
+                string snippet = $@" NOT NULL";
+                codeBuilder.InsertCode(snippet, PropertyCodeGenerator.DbDeltaPropertyColumnTag, info.Property);
+            }
+        }
+    }
+}
---
+++ KMeze.WP.DSL/Plugins/Dsl/SimpleBusinessLogic/RequiredInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("Required")]
+    public class RequiredInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public PropertyInfo Property { get; set; }
+    }
+}
