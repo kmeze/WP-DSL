9eea60c0df3c18f8b346f59e7a804321110e09b8
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-06 18:26:13 +0100

    Added UniqueMultiple concept

---
+++ KMeze.WP.DSL/Plugins/Dom/SimpleBusinessLogic/UniqueMultipleCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(UniqueMultipleInfo))]
+    public class UniqueMultipleCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (UniqueMultipleInfo)conceptInfo;
+
+            string snippet = $@",UNIQUE key_{info.Entity.Name}_{info.Columns.Trim().Replace(" ", "").Replace(",", "_")} ({info.Columns.Trim()})
+                        ";
+            codeBuilder.InsertCode(snippet, EntityDbDeltaCodeGenerator.DbDeltaKeyTag, info.Entity);
+        }
+    }
+}
