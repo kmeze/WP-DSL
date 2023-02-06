be8b8a22e7efa66cb9a2b8453f5225c4d5c1fd4e
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-06 17:27:09 +0100

    Added Unique concept

---
+++ KMeze.WP.DSL/Plugins/Dom/SimpleBusinessLogic/UniqueCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(UniqueInfo))]
+    public class UniqueCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (UniqueInfo)conceptInfo;
+
+            string snippet = $@",UNIQUE key_{info.Property.DataStructure.Name}_{info.Property.Name} ({info.Property.Name})
+                        ";
+            codeBuilder.InsertCode(snippet, EntityDbDeltaCodeGenerator.DbDeltaKeyTag, info.Property.DataStructure);
+        }
+    }
+}
