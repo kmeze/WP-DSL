0a9533f3728358c01cb37aa13fea3250692eccb9
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-06 10:24:05 +0100

    Added DefaultValue concept

---
+++ KMeze.WP.DSL/Plugins/Dom/SimpleBusinessLogic/DefaultValueCodeGnerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+using KMeze.WP.DSL;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(DefaultValueInfo))]
+    public class DefaultValueCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (DefaultValueInfo)conceptInfo;
+
+            if (info.Property.DataStructure is EntityInfo)
+            {
+                string snippet = $@" DEFAULT {info.Value}";
+                codeBuilder.InsertCode(snippet, PropertyCodeGenerator.DbDeltaPropertyColumnTag, info.Property);
+            }
+        }
+    }
+}
---
+++ KMeze.WP.DSL/Plugins/Dsl/SimpleBusinessLogic/DefaultValueInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("DefaultValue")]
+    public class DefaultValueInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public PropertyInfo Property { get; set; }
+
+        [ConceptKey]
+        public string Value { get; set; }
+    }
+}
