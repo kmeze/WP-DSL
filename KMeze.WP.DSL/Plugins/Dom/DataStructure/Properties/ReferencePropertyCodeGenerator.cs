e20874211f2d06b0289ed297ba041fcb54b45c5f
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-06 16:11:38 +0100

    Add foreign ket for ReferencePropertyInfo

--- KMeze.WP.DSL/Plugins/Dom/DataStructure/Properties/ReferencePropertyCodeGenerator.cs
+++ KMeze.WP.DSL/Plugins/Dom/DataStructure/Properties/ReferencePropertyCodeGenerator.cs
@@ -31,6 +31,24 @@ namespace KMeze.WP.DSL
                 snippet = $@",KEY ind_{info.DataStructure.Name}_{info.Name}_id ({info.Name}_id)
                         ";
                 codeBuilder.InsertCode(snippet, EntityDbDeltaCodeGenerator.DbDeltaKeyTag, info.DataStructure);
+
+                // Fast hack to enable adding reference to wp_users table
+                string referencedTable = $@"{info.ReferencedDataStructure.WPPlugin.Name}_{info.ReferencedDataStructure.Name}";
+                if (info.ReferencedDataStructure is WPDataStructureInfo && info.ReferencedDataStructure.Name == "User") referencedTable = "users";
+
+                snippet = $@"$referenced_table_name = $wpdb->prefix . '{referencedTable}';
+    $db_name               = DB_NAME;
+	$key_name              = ""fk_{info.DataStructure.Name}_{info.Name}_id"";
+	$sql                   = ""SELECT CONSTRAINT_NAME FROM information_schema.TABLE_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = '$db_name' AND CONSTRAINT_NAME = '$key_name' AND CONSTRAINT_TYPE = 'FOREIGN KEY';"";
+
+	if ( is_null( $wpdb->get_var( $sql ) ) ) {{
+		$res = $wpdb->query( ""ALTER TABLE $table_name ADD CONSTRAINT $key_name FOREIGN KEY ({info.Name}_id) REFERENCES $referenced_table_name (ID) ON DELETE CASCADE ON UPDATE CASCADE;"" );
+
+		if ( ! (bool) $res ) throw new Exception( $wpdb->last_error );
+	}}
+
+    ";
+                codeBuilder.InsertCode(snippet, EntityDbDeltaCodeGenerator.DbDeltaAfterTag, info.DataStructure);
             }
         }
     }
