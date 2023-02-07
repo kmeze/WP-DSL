beaddac0274eddb5a6bc0b3df5951c7fd94976d6
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-07 07:26:50 +0100

    Added MaxLength concept

---
+++ KMeze.WP.DSL/Plugins/Dom/SimpleBusinessLogic/MaxLengthCodeGnerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+using KMeze.WP.DSL;
+
+namespace KMeze.WP.DSL
+{
+    [Export(typeof(IWPPluginConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(MaxLengthInfo))]
+    public class MaxLengthCodeGenerator : IWPPluginConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (MaxLengthInfo)conceptInfo;
+
+            if (info.Property.DataStructure is EntityInfo)
+            {
+                string snippet = $@" ({info.Value})";
+                codeBuilder.InsertCode(snippet, PropertyCodeGenerator.DbDeltaPropertyColumnTag, info.Property);
+            }
+        }
+    }
+}
