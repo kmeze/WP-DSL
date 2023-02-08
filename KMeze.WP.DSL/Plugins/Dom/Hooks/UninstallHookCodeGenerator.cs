01ae412c958abfd946580b77613b8881e5306c9d
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-08 10:14:02 +0100

    Properties renamed

--- KMeze.WP.DSL/Plugins/Dom/Hooks/UninstallHookCodeGenerator.cs
+++ KMeze.WP.DSL/Plugins/Dom/Hooks/UninstallHookCodeGenerator.cs
@@ -15,10 +15,10 @@ namespace KMeze.WP.DSL
         {
             var info = (UninstallHookInfo)conceptInfo;
 
-            string snippet = $@"{info.WPPlugin.Name}_{info.Action.Name} ();
+            string snippet = $@"{info.Plugin.Name}_{info.Callback.Name} ();
 
     ";
-            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.UninstallHookTag, info.WPPlugin);
+            codeBuilder.InsertCode(snippet, WPPluginCodeGenerator.UninstallHookTag, info.Plugin);
         }
     }
 }
