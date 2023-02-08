a5574b566d68c691190dfc1f5639879d4eb79509
Kresimir Meze <kresimir.meze@gmail.com>
2023-02-08 13:01:20 +0100

    ActionHookMacros moved to ActionHookInfo file

--- KMeze.WP.DSL/Plugins/Dsl/Hooks/ActionHookInfo.cs
+++ KMeze.WP.DSL/Plugins/Dsl/Hooks/ActionHookInfo.cs
@@ -22,4 +22,75 @@ namespace KMeze.WP.DSL
         [ConceptKey]
         public string Args { get; set; }
     }
+
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("ActionHook")]
+    public class ActionHookWithoutArgsInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public WPPluginInfo Plugin { get; set; }
+
+        [ConceptKey]
+        public string Hook { get; set; }
+
+        [ConceptKey]
+        public CallbackInfo Callback { get; set; }
+
+        [ConceptKey]
+        public string Priority { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class ActionHookWithoutMacro : IConceptMacro<ActionHookWithoutArgsInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(ActionHookWithoutArgsInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            newConcepts.Add(new ActionHookInfo
+            {
+                Plugin = conceptInfo.Plugin,
+                Hook = conceptInfo.Hook,
+                Callback = conceptInfo.Callback,
+                Priority = conceptInfo.Priority,
+                Args = "",
+            });
+
+            return newConcepts;
+        }
+    }
+
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("ActionHook")]
+    public class ActionHookWithDefaultPriorityInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public WPPluginInfo Plugin { get; set; }
+
+        [ConceptKey]
+        public string Hook { get; set; }
+
+        [ConceptKey]
+        public CallbackInfo Callback { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class ActionHookWithDefaultPriorityMacro : IConceptMacro<ActionHookWithDefaultPriorityInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(ActionHookWithDefaultPriorityInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            newConcepts.Add(new ActionHookInfo
+            {
+                Plugin = conceptInfo.Plugin,
+                Hook = conceptInfo.Hook,
+                Callback = conceptInfo.Callback,
+                Priority = "DefaultPriority",
+                Args = "",
+            });
+
+            return newConcepts;
+        }
+    }
 }
